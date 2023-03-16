c0b93481b6566286052c8927e17e7bda9eed083e
VanessJames <vanessjames10@gmail.com>
2023-03-16 12:20:57 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main() {
-  char message[] = "Programming is like building a multilingual puzzle";
-  puts(message);
-  return 0;
+int main(void)
+{
+    char message[] = "Programming is like building a multilingual puzzle";
+    puts(message);
+    return (0);
 }
-
