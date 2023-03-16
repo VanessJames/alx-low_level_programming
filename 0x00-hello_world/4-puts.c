5c435a1d849d746a50247f6057bce2ba61ec6739
VanessJames <vanessjames10@gmail.com>
2023-03-16 12:57:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main() {
-  char message[] =("\"Programming is like building a multilingual puzzle);
-  puts(message);
-  return 0;
-}
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
