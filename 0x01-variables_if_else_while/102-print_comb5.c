be27854bb39c5d8ee1cb85c0e48b674905e570a5
VanessJames <vanessjames10@gmail.com>
2023-03-17 19:30:26 -0700

    loki

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,11 +26,13 @@ int main(void)
 			putchar(n3 + '0');
 			putchar(n4 + '0');
 			if (i == 98 && j == 99)
+			continue;
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
