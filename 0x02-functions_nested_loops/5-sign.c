58cada33f1fe47e5a87fa40f4d4129850b4818d6
VanessJames <vanessjames10@gmail.com>
2023-03-21 13:15:21 -0700

    kl

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - prints sign of numbers
  *
- * @n: parameter
+ * @n: functioin parameter
  *
  * Return: 1 if n is greater than zero,
  * 0 if n if zero and -1 if n is less than zero
@@ -13,22 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(0);
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 
