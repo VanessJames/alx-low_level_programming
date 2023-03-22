df42716c5ade28adf98af55d2904ed5bd9e377b3
VanessJames <vanessjames10@gmail.com>
2023-03-21 18:45:08 -0700

    lol

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,28 @@
 
 void times_table(void)
 {
-	int x;
-	int y;
-	int z;
-	int u;
-	int d;
+	int a, b, op;
 
-	for (x = 0 ; x <= 9 ; x++)
+
+	for (a = 0; a <= 9; a++)
 	{
-		for (y = 0 ; y <= 9 ; y++)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
-			z = x * y;
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-			_putchar(u + '0');
-			}
-			else
-			{
+			op = a * b;
 			_putchar(44);
 			_putchar(32);
-			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+				}
+				else
+				{
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 		}
-		_putchar(z + '0');
-	}
 		_putchar('\n');
 	}
 }
