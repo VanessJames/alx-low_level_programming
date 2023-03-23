26d6fce896839d9190e550907e4a5c7f8e5fa5ca
VanessJames <vanessjames10@gmail.com>
2023-03-23 14:51:35 -0700

    juj

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,11 +13,13 @@ int main(void)
 
 	n = 612852475143;
 	max = -1;
+
 	while (n % 2 == 0)
 	{
 		max = 2;
-		n / = 2;
+		n /= 2;
 	}
+
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 	while (n % i == 0)
@@ -26,8 +28,10 @@ int main(void)
 		n = n / i;
 	}
 	}
+
 	if (n > 2)
-		max = n;
+	max = n;
+
 		printf("%ld\n", max);
 
 	return (0);
