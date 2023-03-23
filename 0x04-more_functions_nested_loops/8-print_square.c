eaebaa77f3b18bbac4d7574d319c3883bf581f1a
VanessJames <vanessjames10@gmail.com>
2023-03-23 14:39:56 -0700

    oopo

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,16 @@ void print_square(int size)
 		_putchar('\n');
 	}
 	else
-
+	{
 	int i, j;
 
 	for (i = 0; i < size; i++)
+	{
 	for (j = 0; j < size; j++)
 	{
 		_putchar('#');
 	}
-	{
 		_putchar('\n');
+	}
 	}
 }
