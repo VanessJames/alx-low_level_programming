982fcfcac9bb512a7a052217e3c4491b3c0e76a0
VanessJames <vanessjames10@gmail.com>
2023-03-23 14:25:45 -0700

    opi

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,18 +5,18 @@
  */
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
+	{
+	int i;
+
 	for (i = 1; i <= n; i++)
 	{
 		_putchar('_');
 	}
-	{
 	_putchar('\n');
 	}
 }
