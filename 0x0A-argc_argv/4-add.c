dc8c525d6eedb48a57066e124b883de25115f20e
VanessJames <vanessjames10@gmail.com>
2023-04-09 12:40:08 -0700

    lpo

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
+	int i = 1;
 
 	if (argc == 1)
 	{
@@ -17,23 +18,25 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (int i = 1; i < argc; i++)
+	while (i < argc)
 	{
 		int j = 0;
 
-		while (argv[i][j] != '\0')
+	while (argv[i][j] != '\0')
 		{
-			if (!isdigit((unsigned char) argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
+
 			j++;
 		}
+
 		sum += atoi(argv[i]);
+		i++;
 	}
 
 	printf("%d\n", sum);
-
 	return (0);
 }
