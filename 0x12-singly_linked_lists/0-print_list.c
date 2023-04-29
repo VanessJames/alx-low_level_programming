7c5379d1957b70cd2483863c669423979d5aa591
VanessJames <vanessjames10@gmail.com>
2023-04-28 17:52:14 -0700

    fixing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t ka = 0;
 
 	while (h != NULL)
 	{
 		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
-		count++;
+		ka++;
 	}
 
-	return (count);
+	return (ka);
 }
