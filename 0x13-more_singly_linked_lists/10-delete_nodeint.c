0a087161c5875b0aeb5f7c82e3d610e750949a6a
VanessJames <vanessjames10@gmail.com>
2023-05-02 17:01:05 -0700

    #10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	prev = NULL;
 
 
-	for (i = 0; current != NULL && i > index; i++)
+	for (i = 0; current != NULL && i < index; i++)
 	{
 		prev = current;
 		current = current->next;
